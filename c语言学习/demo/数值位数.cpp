e5942d6929c7d1327575273ac7a4549b64ee3dbf
helloforrestworld <dongweijia238@gmail.com>
2018-08-19 21:18:02 +0800

    c语言基础练习

--- c语言学习/demo/数值位数.cpp
+++ c语言学习/demo/数值位数.cpp
@@ -2,7 +2,7 @@
 int main() {
 	int x;
 	int n = 0;
-	printf("请输入一个整数\n"); 
+	printf("请输入一个正整数\n"); 
 	scanf("%d", &x);
 	printf("你输入的数字是%d", x);
 	while(x > 0) {
@@ -10,4 +10,19 @@ int main() {
 		x /= 10;
 	}
 	printf(",它是个%d位数", n);
+	return 0; 
 }
+
+//int main2() {
+//	int x;
+//	int n = 0;
+//	printf("请输入一个正整数\n"); 
+//	scanf("%d", &x);
+//	// 0的情况 
+//	do {
+//		n++;
+//		x /= 10;	
+//	} while(x >0);
+//	printf(",它是个%d位数", n);
+//	return 0; 
+//}
