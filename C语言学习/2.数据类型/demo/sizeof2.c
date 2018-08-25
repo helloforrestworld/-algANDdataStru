9dfda49dbdcd1b6c934c23caf61ad8c82beeca66
helloforrestworld <dongweijia238@gmail.com>
2018-08-25 19:03:26 +0800

    bit

---
+++ C语言学习/2.数据类型/demo/sizeof2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+	printf("sizeof(char)=%d\n", sizeof(char));
+	printf("sizeof(short)=%d\n", sizeof(short));
+	printf("sizeof(int)=%d\n", sizeof(int));
+	printf("sizeof(long)=%d\n",sizeof(long));
+	printf("sizeof(long long)=%d\n", sizeof(long long));
+	return 0;
+} 
